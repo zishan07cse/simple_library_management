27047e83dbb2cc5bf291e859c8187aaa6a0efcfc
zishan07cse <zishan07csecuet@gmail.com>
2024-03-29 02:19:11 +0600

    Fix:previous logo is replaced and style file is changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
             <div class="row">
                 <div class="offset-md-4 col-md-4 login_div">
                     <div class="logo mt-30">
-                        <img src="{{ asset('images/logo.jpg') }}" alt="">
+                        <img src="{{ asset('images/logo_login.png') }}" alt="" class="logo_img">
                     </div>
-                    <form class="yourform mt-50" action="{{ route('login') }}" method="post">
+                    <form class="yourform mt-20" action="{{ route('login') }}" method="post">
                         @csrf
                         <h3 class="heading">Admin Login</h3>
                         <div class="form-group">
